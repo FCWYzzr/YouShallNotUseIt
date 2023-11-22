7d84f12f707818fd19519b0c28d1dc50946d1d90
FCWYzzr <fcwyzzr@163.com>
2023-11-22 13:03:46 +0800

    initial commit

--- magicians/Locking.py
+++ magicians/Locking.py
@@ -44,10 +44,18 @@ def lock(obj: Union[BIF, type]):
             raise ValueError("given obj is not in module builtins")
         TypeLocker.lock(obj.__name__)
 
+    elif isinstance(obj, str):
+        raise ValueError("a type or a function? param should be BIF or BIT, not types")
+
+    elif obj == LockedFunction or obj == LockedType:
+        return
+
     else:
         raise ValueError("Only support lock BIF or type")
 
 
 def unlock(name: str):
+    if not isinstance(name, str):
+        raise ValueError("param shall be a string, try again.")
     FunctionLocker.unlock(name)
     TypeLocker.unlock(name)
